0c5edad3d5c356c75d71c4b390821ceade97a9e6
Vlad Andreev <andreevlad@gmail.com>
2016-11-22 13:34:31 +0600

    Add TRAVIS_BUILD_NUMBER variable for tag docker image

--- sm-push.sh
+++ sm-push.sh
@@ -3,24 +3,15 @@
 function tag_and_push {
 	if [ -n "$1" ] && [ -n "$IMAGE_NAME" ]; then
 		echo "Pushing docker image to hub tagged as $IMAGE_NAME:$1"
-		docker build -t $IMAGE_NAME:$1 .
-		docker push $IMAGE_NAME:$1
+		docker build -t $IMAGE_NAME:$1 -t $IMAGE_NAME .
+		docker push $IMAGE_NAME:$1 $IMAGE_NAME
 	fi
 }
 
-LATEST_TAG="latest"
-MAJOR_TAG=""
-VERSION_TAG=""
+VERSION_TAG=v.$TRAVIS_BUILD_NUMBER
 
-if [ -n "$TRAVIS_TAG" ] && [[ "${TRAVIS_TAG}" =~ ([0-9]+). ]]; then
-	VERSION_TAG=v$TRAVIS_TAG
-	if [[ $TRAVIS_TAG != *"rc"* ]]; then
-		MAJOR_TAG=v${BASH_REMATCH[1]}
-	fi
-fi
 
-if [ "${TRAVIS_GO_VERSION}" = "${GO_FOR_RELEASE}" ]; then
-	cat > ~/.dockercfg <<EOF
+cat > ~/.dockercfg <<EOF
 {
   "https://index.docker.io/v1/": {
     "auth": "${HUB_AUTH}",
@@ -28,12 +19,5 @@ if [ "${TRAVIS_GO_VERSION}" = "${GO_FOR_RELEASE}" ]; then
   }
 }
 EOF
-	if [ -n "$TRAVIS_TAG" ]; then
-		tag_and_push $MAJOR_TAG
-		tag_and_push $VERSION_TAG
-	elif [ "${TRAVIS_BRANCH}" = "master" ]; then
-		tag_and_push $LATEST_TAG
-	fi
-else
-	echo "No image to build"
-fi
+
+tag_and_push $VERSION_TAG
