7f54bf3345900288438efccdea3ea8d495ee5e13
Vlad Andreev <andreevlad@gmail.com>
2016-11-22 13:46:09 +0600

    Add check for GO version

--- sm-push.sh
+++ sm-push.sh
@@ -3,21 +3,25 @@
 function tag_and_push {
 	if [ -n "$1" ] && [ -n "$IMAGE_NAME" ]; then
 		echo "Pushing docker image to hub tagged as $IMAGE_NAME:$1"
-		docker build -t $IMAGE_NAME:$1 -t $IMAGE_NAME .
+		docker build -t $IMAGE_NAME:$1 -t $IMAGE_NAME -f Dockerfile_Travis .
 		docker push $IMAGE_NAME:$1 $IMAGE_NAME
 	fi
 }
 
 VERSION_TAG=v.$TRAVIS_BUILD_NUMBER
 
+if [ "${TRAVIS_GO_VERSION}" = "${GO_FOR_RELEASE}" ]; then
+	cat > ~/.dockercfg <<EOF
+	{
+	  "https://index.docker.io/v1/": {
+	    "auth": "${HUB_AUTH}",
+	    "email": "${HUB_EMAIL}"
+	  }
+	}
+	EOF
 
-cat > ~/.dockercfg <<EOF
-{
-  "https://index.docker.io/v1/": {
-    "auth": "${HUB_AUTH}",
-    "email": "${HUB_EMAIL}"
-  }
-}
-EOF
+	tag_and_push $VERSION_TAG
+else
+	echo "No image to build"
+fi
 
-tag_and_push $VERSION_TAG
